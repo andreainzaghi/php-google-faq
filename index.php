bad5a5ec098283a529c07d28716770129315c0d1
Andrea <andrea3aprile99@gmail.com>
2021-05-04 16:00:21 +0200

    s

--- index.php
+++ index.php
@@ -8,6 +8,16 @@ Per presentare una richiesta di rimozione, compila questo modulo web. Riceverai 
 Nei prossimi mesi lavoreremo a stretto contatto con le autorità per la protezione dei dati e con altre autorità per perfezionare il nostro approccio. La decisione della CGUE rappresenta un cambiamento significativo per i motori di ricerca. Siamo preoccupati per le sue conseguenze, ma riteniamo anche che sia importante rispettare la decisione della Corte e ci stiamo adoperando per predisporre una procedura conforme alla legge.
 Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i risultati potrebbero essere stati modificati nel rispetto delle leggi europee per la protezione dei dati. Mostriamo questa notifica agli utenti europei quando cercano la maggior parte dei nomi, non soltanto le pagine che sono state soggette a una rimozione."
  ],
+ [
+   "domande"=>"Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?",
+    "risposte"=>"Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un'elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.
+
+Siamo costantemente al lavoro per garantire un'elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te. Spendiamo centinaia di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.
+
+Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel Centro Google per la sicurezza online.
+
+Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo."
+ ]
 ];
 ?>
 <!DOCTYPE html>
@@ -17,19 +27,13 @@ Quando cerchi un nome, potresti trovare una notifica in cui viene spiegato che i
     <title></title>
   </head>
   <body>
-    <h1>
+    <h2>
 <?php
  foreach ($people as $key) {
       // code...
-      echo "{$key['domande']} ";
+      echo "{$key['domande']} <br> {$key['risposte']} <br> ";
     } ?>
-    </h1>
-    <p>
-      <?php
-      foreach ($people as $key) {
-          // code...
-          echo " {$key['risposte']} ";
-        } ?>
-      </p>
+  </h2>
+
   </body>
 </html>
